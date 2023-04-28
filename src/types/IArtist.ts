df5a349f2ea1de3b3ad07956add12d5b0da6ebbb
i.karshev <guchimister@gmail.com>
2023-04-28 14:27:08 +0400

    - fixed types and store

--- src/types/IArtist.ts
+++ src/types/IArtist.ts
@@ -1,41 +1,20 @@
 import { IImage } from './IImage';
 
-<<<<<<<<< Temporary merge branch 1
-export interface IArtistStatic {
-=========
 export interface Artist {
->>>>>>>>> Temporary merge branch 2
   genres: string[];
   _id: string;
   name: string;
   description: string;
   yearsOfLife: string;
-<<<<<<<<< Temporary merge branch 1
-  __v?: number;
-  mainPainting: {
-    _id: string;
-    name: string;
-    yearOfCreation: number;
-    image: IImage;
-    artist: string;
-  };
-}
-
-export interface IArtist {
-=========
   avatar?: IImage;
 }
 
 export interface IArtistStatic {
->>>>>>>>> Temporary merge branch 2
   genres: string[];
   _id: string;
   name: string;
   description: string;
   yearsOfLife: string;
-<<<<<<<<< Temporary merge branch 1
-  avatar?: IImage;
-=========
   __v?: number;
   mainPainting: {
     _id: string;
@@ -44,5 +23,4 @@ export interface IArtistStatic {
     image: IImage;
     artist: string;
   };
->>>>>>>>> Temporary merge branch 2
 }
