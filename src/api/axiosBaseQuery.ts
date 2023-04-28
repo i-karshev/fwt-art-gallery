403270035150872666e2e6254770a77514680d17
i.karshev <guchimister@gmail.com>
2023-04-28 13:52:36 +0400

    - add axios instance

--- src/api/axiosBaseQuery.ts
+++ src/api/axiosBaseQuery.ts
@@ -1,33 +1,20 @@
 import { BaseQueryFn } from '@reduxjs/toolkit/query';
 import axios from 'axios';
 import type { AxiosRequestConfig, AxiosError } from 'axios';
+import { API_BASE_URL } from '../constans';
+
+const instance = axios.create({
+  baseURL: API_BASE_URL,
+});
 
 export const axiosBaseQuery =
-  (
-    { baseUrl }: { baseUrl: string } = { baseUrl: '' }
-  ): BaseQueryFn<
-    {
-      url: string;
-      method: AxiosRequestConfig['method'];
-      headers?: AxiosRequestConfig['headers'];
-      data?: AxiosRequestConfig['data'];
-      params?: AxiosRequestConfig['params'];
-    },
-    unknown,
-    unknown
-  > =>
-  async ({ url, method, headers, data, params }) => {
+  <T>(): BaseQueryFn<AxiosRequestConfig, T> =>
+  async (config: AxiosRequestConfig<T>) => {
     try {
-      // eslint-disable-next-line @typescript-eslint/no-unsafe-assignment
-      const result = await axios({ url: baseUrl + url, method, headers, data, params });
-      return { data: result.data };
+      return await instance(config);
     } catch (axiosError) {
-      const err = axiosError as AxiosError;
       return {
-        error: {
-          status: err.response?.status,
-          data: err.response?.data || err.message,
-        },
+        error: axiosError as AxiosError,
       };
     }
   };
--- src/api/index.ts
+++ src/api/index.ts
@@ -1,10 +1,7 @@
 import { createApi } from '@reduxjs/toolkit/query/react';
 import { axiosBaseQuery } from './axiosBaseQuery';
-import { API_BASE_URL } from '../constans';
 
 export const apiService = createApi({
-  baseQuery: axiosBaseQuery({
-    baseUrl: API_BASE_URL,
-  }),
+  baseQuery: axiosBaseQuery(),
   endpoints: () => ({}),
 });
