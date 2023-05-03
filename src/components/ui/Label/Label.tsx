faae5459a5cd7738da089878ffced3619b40cd83
i.karshev <guchimister@gmail.com>
2023-05-03 14:09:57 +0400

    + add Label component

---
+++ src/components/ui/Label/Label.tsx
@@ -0,0 +1,25 @@
+import React, { FC } from 'react';
+import cn from 'classnames/bind';
+
+import { ReactComponent as CloseIcon } from '@/assets/svg/close_icon_small.svg';
+import styles from './Label.module.scss';
+
+const cx = cn.bind(styles);
+
+interface LabelProps {
+  name: string;
+  onClose?: () => void;
+  isDarkTheme?: boolean;
+}
+
+export const Label: FC<LabelProps> = ({ name, onClose, isDarkTheme }) => (
+  <div className={cx('label', { label_dark: isDarkTheme })}>
+    <span className={cx('label__text')}>{name}</span>
+
+    {!!onClose && (
+      <span role="presentation" className={cx('label__icon')} onClick={onClose}>
+        <CloseIcon />
+      </span>
+    )}
+  </div>
+);
